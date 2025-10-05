0fd8ec412bb12bc2f5d4b38bff588e5e8bb8bd72
Lychagin228 <anton228lychagin@gmail.com>
2025-10-05 19:16:30 +0300

    Add files via upload
    Выполнил Лычагин Антон. бИЦ-252

---
+++ Dz4.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_CTYPE, "RUS");
+
+    int A, B, C;
+
+    printf("Введите значения параметров A, B, C: ");
+    scanf("%d %d %d", &A, &B, &C);
+
+    if (A % 3 == 0 && B % 3 == 0 && C % 3 == 0) {
+        printf("КАЛИБРОВКА УСПЕШНА\n");
+    }
+    else {
+        printf("КАЛИБРОВКА НЕУДАЧНА\n");
+    }
+
+    return 0;
+}
